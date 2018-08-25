769239edb8ce194b5ac4f375d157bf9c986ceb0f
Marta Piątek <martusia.piatek@gmail.com>
2018-08-25 20:49:07 +0200

    Dodanie animacji przejść

--- skrypty/main.js
+++ skrypty/main.js
@@ -1,12 +1,13 @@
 var SELEKTOR_DUZEGO_OBRAZU = '[data-typ-obrazu="cel"]';
 var SELEKTOR_TYTULU_OBRAZU = '[data-typ-obrazu="tytul"]';
 var SELEKTOR_MINIATURY = '[data-typ-obrazu="wyzwalacz"]';
+var SELEKTOR_RAMKI_OBRAZU = '[data-typ-obrazu="ramka"]';
 
 var KLASA_UKRYTY_DUZY_OBRAZ = 'ukryty-duzy-obraz';
-
+var REGULA_MALEGO_OBRAZU = 'bardzo-maly-obraz';
 var KLAWISZ_ESC = 27;
 
-function zmienObraz(urlObrazu, tekstTytulu){
+function zmienObraz(urlObrazu, tekstTytulu) {
   'use strict';
   var duzyObraz = document.querySelector(SELEKTOR_DUZEGO_OBRAZU);
   duzyObraz.setAttribute('src', urlObrazu);
@@ -14,22 +15,22 @@ function zmienObraz(urlObrazu, tekstTytulu){
   tytulObrazu.textContent = tekstTytulu;
 }
 
-function obrazMiniatury(miniatura){
+function obrazMiniatury(miniatura) {
   'use strict';
   return miniatura.getAttribute('data-url-obrazu');
 }
 
-function tytulMiniatury(miniatura){
+function tytulMiniatury(miniatura) {
   'use strict';
   return miniatura.getAttribute('data-tytul-obrazu');
 }
 
-function zmienObrazNaPodstMiniatury(miniatura){
+function zmienObrazNaPodstMiniatury(miniatura) {
   'use strict';
   zmienObraz(obrazMiniatury(miniatura), tytulMiniatury(miniatura));
 }
 
-function dodajObslugeKliknieciaMiniatury(miniatura){
+function dodajObslugeKliknieciaMiniatury(miniatura) {
   'use strict';
   miniatura.addEventListener('click', function(zdarzenie) {
     zdarzenie.preventDefault();
@@ -38,35 +39,41 @@ function dodajObslugeKliknieciaMiniatury(miniatura){
   });
 }
 
-function odczytajTabliceMiniatur(){
+function odczytajTabliceMiniatur() {
   'use strict';
   var miniatury = document.querySelectorAll(SELEKTOR_MINIATURY);
   var tablicaMiniatur = [].slice.call(miniatury);
   return tablicaMiniatur;
 }
 
-function ukryjDuzyObraz(){
+function ukryjDuzyObraz() {
   'use strict';
   document.body.classList.add(KLASA_UKRYTY_DUZY_OBRAZ);
 }
 
-function pokazDuzyObraz(){
+function pokazDuzyObraz() {
   'use strict';
+  var ramka = document.querySelector(SELEKTOR_RAMKI_OBRAZU);
   document.body.classList.remove(KLASA_UKRYTY_DUZY_OBRAZ);
+  ramka.classList.add(REGULA_MALEGO_OBRAZU);
+  setTimeout(function () {
+    ramka.classList.remove(REGULA_MALEGO_OBRAZU);
+  }, 50);
+
 }
 
-function dodajObslugeKlawiszy(){
+function dodajObslugeKlawiszy() {
   'use strict';
   document.body.addEventListener('keyup', function(event) {
     event.preventDefault();
     console.log(event.keyCode);
-    if(event.keyCode === KLAWISZ_ESC){
+    if (event.keyCode === KLAWISZ_ESC) {
       ukryjDuzyObraz();
     }
   });
 }
 
-function inicjujZdarzenia(){
+function inicjujZdarzenia() {
   'use strict';
   var miniatury = odczytajTabliceMiniatur();
   miniatury.forEach(dodajObslugeKliknieciaMiniatury);
